decff30ebb375ffd1071faa521d5136354563e77
Kyrius <88332280+uKyrius@users.noreply.github.com>
2021-08-02 16:05:53 -0300

    Add files via upload

---
+++ BOT.py
@@ -0,0 +1,19 @@
+from instabot import Bot
+import glob, os
+
+try: 
+	os.remove('config/' + USER + '_uuid_and_cookie.json')
+except:
+    pass
+    
+bot = Bot ()
+
+USER = ('aa')
+PASS = ('aa')
+
+bot.login(username = (USER), password = (PASS))
+
+os.chdir("images")
+for file in glob.glob("*.jpg"):
+	bot.upload_photo(file, caption = "#instagram #instagood #love #like #follow #photography #photooftheday #instadaily #likeforlikes #picoftheday #fashion #bhfyp #beautiful #instalike #me #likes #followforfollowback #smile #followme #myself #art #photo #happy #style #likeforfollow #life #l #nature #followers #bhfyp")
+	
